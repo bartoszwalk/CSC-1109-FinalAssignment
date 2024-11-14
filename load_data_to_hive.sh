e08d292c13154550fdc48261e7f8d774b74bb1df
Bartosz Walkowiak <bartoszwalk@gmail.com>
2024-11-14 14:25:31 +0000

    Add script for loading and cleanup

---
+++ load_data_to_hive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+HDFS_DIR="/user/hive/warehouse/cleaned_accidents"
+LOCAL_DIR="results/cleaned_accidents"
+
+echo "Creating HDFS directory: $HDFS_DIR"
+hdfs dfs -mkdir -p $HDFS_DIR
+
+echo "Copying data from $LOCAL_DIR to $HDFS_DIR"
+hdfs dfs -put $LOCAL_DIR/* $HDFS_DIR
+
+echo "Listing files in HDFS directory $HDFS_DIR"
+hdfs dfs -ls $HDFS_DIR
+
+echo "Data loading completed."
